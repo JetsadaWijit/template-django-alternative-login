18c38cd96e0269faa947c1e696b3bcee2e05a0ab
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-06-28 12:46:47 +0700

    Create setup.sh

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Update pip (recommended for security and compatibility)
+python -m pip install --upgrade pip
+
+# Install dependencies from requirements.txt
+pip install -r requirements.txt
+
+echo "Setup complete!"
